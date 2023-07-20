5c4edcab94cd3b0212664ce4a67a58c73bc98042
ktoufighi <kathy.toufighi@gmail.com>
2023-07-19 21:51:16 -0400

    unistall fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -126,9 +126,7 @@ class RecipeView extends View {
       <svg class="recipe__icon">
         <use href="${icons}#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${
-        item.quantity ? new Fraction(item.quantity).toString() : ''
-      }</div>
+      <div class="recipe__quantity">${item.quantity}</div>
       <div class="recipe__description">
         <span class="recipe__unit">${item.unit}</span>
         ${item.description}
